50fa9c8e09b24ce1dabfdf1c225c8a470ee48b52
Yuri Yaryshev <yuyaryshev@gmail.com>
2025-11-16 17:14:35 +0300

    Before publishing 1.3.0

--- control.lua
+++ control.lua
@@ -12,10 +12,41 @@ local function dbgPrint(s)
 --    end
 end
 
+
+local additional_suffixes = {
+	"-recycling-comb",
+	"-extended",
+    "-all-except-blacklisted", 
+	"-all",
+	"-custom-1",
+	"-custom-2",
+	"-custom-3",
+}
+
+local recipeCombinatorNames = {
+	["lo-recipe-combinator-recycling-comb"] = true,
+	["lo-recipe-combinator-extended"] = true,
+    ["lo-recipe-combinator-all-except-blacklisted"] = true, 
+	["lo-recipe-combinator-all"] = true,
+	["lo-recipe-combinator-custom-1"] = true,
+	["lo-recipe-combinator-custom-2"] = true,
+	["lo-recipe-combinator-custom-3"] = true,
+	
+	["lo-recipe-combinator-recycling-comb-packed"] = true,
+	["lo-recipe-combinator-extended-packed"] = true,
+    ["lo-recipe-combinator-all-except-blacklisted-packed"] = true, 
+	["lo-recipe-combinator-all-packed"] = true,
+	["lo-recipe-combinator-custom-1-packed"] = true,
+	["lo-recipe-combinator-custom-2-packed"] = true,
+	["lo-recipe-combinator-custom-3-packed"] = true,
+}
+
+
+
 local function ccs_handle_spawned(entityOrEvent, info)
 	local entity = entityOrEvent.valid and entityOrEvent or entityOrEvent.entity
     if not (entity and entity.valid) then return end
-	if entity.name ~= main_name and entity.name ~= packed_name then return end
+	if not recipeCombinatorNames[entity.name] then return end
 	
     local behavior = entity.get_or_create_control_behavior()	
 	behavior.circuit_set_recipe = true
@@ -50,6 +81,7 @@ local function ccs_create_entity(info, surface, position, force)
     return entity
 end
 
+
 local function ccs_init()
     if not (script.active_mods["compaktcircuit"]
         and remote.interfaces["compaktcircuit"]
@@ -70,6 +102,51 @@ local function ccs_init()
 		packed_names = { packed_name },
 		interface_name = main_name
 	})
+	
+	--------------------------------------------------------------------------
+    -- ADDITIONAL SUFFIXES — each gets anonymous create functions
+    --------------------------------------------------------------------------
+    for _, suffix in ipairs(additional_suffixes) do
+        local name = main_name .. suffix
+		if prototypes.entity[name] then
+			local packed = main_name .. suffix..'-packed'
+
+			-- register interface only if not already defined
+			if not remote.interfaces[name] then
+				remote.add_interface(name, {
+					get_info = ccs_get_info,
+
+					create_entity = function(info, surface, position, force)
+						local entity = surface.create_entity{
+							name = name,
+							position = position,
+							force = force,
+							raise_built = false,
+						}
+						ccs_handle_spawned(entity, info)
+						return entity
+					end,
+
+					create_packed_entity = function(info, surface, position, force)
+						local entity = surface.create_entity{
+							name = packed,
+							position = position,
+							force = force,
+							raise_built = false,
+						}
+						ccs_handle_spawned(entity, info)
+						return entity
+					end,
+				})
+			end
+
+			remote.call("compaktcircuit", "add_combinator", {
+				name = name,
+				packed_names = { packed },
+				interface_name = name
+			})
+		end
+	end
 end
 
 script.on_init(ccs_init)
